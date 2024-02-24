de65cd79b5f4dea6f44868710ca87e0034303f60
ShHaWkK <51519814+ShHaWkK@users.noreply.github.com>
2024-02-24 22:04:52 +0100

    Update Room.java

--- main/src/entities/Room.java
+++ main/src/entities/Room.java
@@ -4,23 +4,27 @@ public class Room {
     private int number;
     private Monster monster; // mettre la classe monster 
 
-    public Room(int number) {
+    public Room(int number) 
+    {
         this.number = number;
         //implémenter la logique pour créer et placer un monstre dans la pièce.
     }
 
     // Méthode pour entrer dans la pièce. La logique de combat sera implémentée
-    public void enter(Hero hero) {
+    public void enter(Hero hero) 
+    {
         // Implémentez la logique de rencontre avec le monstre et le combat 
         System.out.println("Entering room " + number);
     }
 
-    public int getNumber() {
+    public int getNumber() 
+    {
         return number;
     }
 
    // une logique pour obtenir le type de monstre.
-    public String getMonsterType() {
+    public String getMonsterType()
+    {
         // Retourne le type de monstre dans la pièce. Vous devez implémenter cette logique.
         return "MonsterTypePlaceholder";
     }
